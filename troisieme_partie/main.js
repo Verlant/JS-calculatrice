a41ce536de97a938373f985a6e12c5f5060db7df
Verlant <antoine.verlyck@gmail.com>
2022-12-06 16:26:13 +0100

    exo a finir

--- troisieme_partie/main.js
+++ troisieme_partie/main.js
@@ -9,18 +9,18 @@ input_affiche.setAttribute("value", "");
 
 // fonction ajouter
 function ajouter(str) {
-  // if (input_affiche.value == undefined || isNaN(Number(input_affiche.value))) {
-  //   input_affiche.value = str;
-  // } else {
-  //   input_affiche.value += str;
-  // }
-  input_affiche.value += str;
+  if (input_affiche.value == undefined || isNaN(Number(input_affiche.value))) {
+    input_affiche.value = str;
+  } else {
+    input_affiche.value += str;
+  }
+  // input_affiche.value += str;
 }
 
 //fonction operation
 function operation(signe) {
-  // left_member = Number(input_affiche.value);
-  // operator = signe;
+  left_member = Number(input_affiche.value);
+  operator = signe;
   input_affiche.value += signe;
 }
 
@@ -48,36 +48,36 @@ function fonctionSpeciale(opespeciale) {
 
 //fonction de resultat
 function resultat() {
-  // right_member = Number(input_affiche.value);
-  // if (right_member == undefined || left_member == undefined) {
-  //   return "";
-  // }
-  // switch (operator) {
-  //   case "+":
-  //     result = left_member + right_member;
-  //     break;
-  //   case "-":
-  //     result = left_member - right_member;
-  //     break;
-  //   case "/":
-  //     result = left_member / right_member;
-  //     break;
-  //   case "*":
-  //     result = left_member * right_member;
-  //     break;
-  //   default:
-  //     break;
-  // }
-  // input_affiche.value = result;
-  // left_member = 0;
-  // right_member = 0;
-  try {
-    input_affiche.value = eval(input_affiche.value);
-  } catch (error) {
-  } finally {
-    alert("Expression incorrecte, réessayez");
-    input_affiche.value = "";
+  right_member = Number(input_affiche.value);
+  if (right_member == undefined || left_member == undefined) {
+    return "";
   }
+  switch (operator) {
+    case "+":
+      result = left_member + right_member;
+      break;
+    case "-":
+      result = left_member - right_member;
+      break;
+    case "/":
+      result = left_member / right_member;
+      break;
+    case "*":
+      result = left_member * right_member;
+      break;
+    default:
+      break;
+  }
+  input_affiche.value = result;
+  left_member = 0;
+  right_member = 0;
+  // try {
+  //   input_affiche.value = eval(input_affiche.value);
+  // } catch (error) {
+  //   alert("Expression incorrecte, réessayez");
+  // } finally {
+  //   input_affiche.value = "";
+  // }
 }
 
 function reset_calculatrice() {
